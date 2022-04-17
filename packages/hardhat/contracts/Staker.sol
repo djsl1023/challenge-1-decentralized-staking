e44ba48e12a6b1bc7166c67d2b8e6de45004e03c
Dan Lee <dan.jisang.lee@gmail.com>
2022-04-17 16:50:12 -0400

    execute, withdraw, timeleft complete

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -14,6 +14,9 @@ contract Staker {
   mapping(address => uint256) public balances;
   //goal threshold
   uint256 public constant threshold = 1 ether;
+  uint256 public deadline = block.timestamp + 30 seconds;
+  bool public openForWithdraw = false;
+  bool public executeCalled = false;
 
   constructor(address exampleExternalContractAddress) public {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
@@ -24,6 +27,7 @@ contract Staker {
   function stake() public payable {
     address sender = msg.sender;
     uint256 amount = msg.value;
+
     balances[sender] += amount;
     emit Stake(sender, amount);
   }
@@ -32,15 +36,34 @@ contract Staker {
 
   // After some `deadline` allow anyone to call an `execute()` function
   //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+  function execute() public {
+    require(block.timestamp >= deadline, "Not time yet");
+    require(!executeCalled, "Already Executed");
+
+    if(address(this).balance >= threshold){
+      exampleExternalContract.complete{value: address(this).balance}();
+    }
+    else{
+      openForWithdraw = true;
+    }
+    executeCalled = true;
+  }
 
 
   // if the `threshold` was not met, allow everyone to call a `withdraw()` function
-
-
   // Add a `withdraw()` function to let users withdraw their balance
-
+  function withdraw() public {
+    require(openForWithdraw, "Can't withdraw at this time");
+    uint256 amount = balances[msg.sender];
+    require(amount > 0, "No funds to withdraw");
+    balances[msg.sender] = 0;
+    payable(msg.sender).transfer(amount);
+  }
 
   // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+  function timeLeft() public view returns(uint256) {
+    return block.timestamp >= deadline ? 0 : deadline - block.timestamp;
+  }
 
 
   // Add the `receive()` special function that receives eth and calls stake()
