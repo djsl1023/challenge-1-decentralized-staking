42982212c0811fdeb0bf1d3a0956147e1b3a48bb
Dan Lee <dan.jisang.lee@gmail.com>
2022-04-17 15:35:10 -0400

    initial stake contract implemented

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -8,12 +8,26 @@ contract Staker {
 
   ExampleExternalContract public exampleExternalContract;
 
+  event Stake(address, uint256);
+
+  //mapping of individual balances
+  mapping(address => uint256) public balances;
+  //goal threshold
+  uint256 public constant threshold = 1 ether;
+
   constructor(address exampleExternalContractAddress) public {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+  function stake() public payable {
+    address sender = msg.sender;
+    uint256 amount = msg.value;
+    balances[sender] += amount;
+    emit Stake(sender, amount);
+  }
+
 
 
   // After some `deadline` allow anyone to call an `execute()` function
